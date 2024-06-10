ebe4a4f5e1376b2362325542438208ffc686956f
Carlos Lazaro <carlos.e.lazaro@hotmail.com>
2024-06-10 15:41:30 -0300

    Incluido Whitelist

--- tienda/settings.py
+++ tienda/settings.py
@@ -53,6 +53,7 @@ THIRD_APPS = [
 INSTALLED_APPS = BASE_APPS + OWN_APPS + THIRD_APPS
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -61,12 +62,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     'corsheaders.middleware.CorsMiddleware',
 ]
 
 
 
 CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+]
+
+CORS_ORIGIN_WHITELIST = [
     'http://localhost:8080',
     'http://127.0.0.1:8080',
 ]
